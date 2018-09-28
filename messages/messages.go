63789ac30bd7c0b2f640adb3b69c8a36421b718f
Romain Gehrig <romain.gehrig@gmail.com>
2018-09-28 15:45:50 +0200

    Add the new message types

--- messages/messages.go
+++ messages/messages.go
@@ -2,20 +2,43 @@ package messages
 
 import "fmt"
 
+///// Client messages
 type Message struct {
 	Text string
 }
 
+///// Internode messages
+/// Status messages
+type PeerStatus struct {
+	Identifier string
+	NextID     uint32
+}
+type StatusPacket struct {
+	Want []PeerStatus
+}
+
+/// Normal messages
 type SimpleMessage struct {
 	OriginalName  string
 	RelayPeerAddr string
 	Contents      string
 }
 
-type GossipPacket struct {
-	Simple *SimpleMessage
+/// Rumor messages
+type RumorMessage struct {
+	Origin string
+	ID     uint32
+	Text   string
 }
 
+//// Actual packet sent
+type GossipPacket struct {
+	Simple *SimpleMessage
+	Rumor  *RumorMessage
+	Status *StatusPacket
+}
+
+/// Print functions
 func (m *Message) String() string {
 	return fmt.Sprintf("CLIENT MESSAGE %s", m.Text)
 }
