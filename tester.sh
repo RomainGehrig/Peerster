3be7a4a5e9559c951222bc47a32938fb4649668f
Romain Gehrig <romain.gehrig@gmail.com>
2018-10-14 13:18:00 +0200

    Add a script to ease peer creation

---
+++ tester.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+DEBUG="true"
+
+UIPort=8080
+gossipPort=2000
+
+i=$1
+name="node$i"
+gossipPort=$(($gossipPort+i))
+peerPort=$((($gossipPort-1)%10+5000))
+UIPort=$(($UIPort+i))
+peer="127.0.0.1:$peerPort"
+gossipAddr="127.0.0.1:$gossipPort"
+echo "./Peerster -UIPort=$UIPort -gossipAddr=$gossipAddr -name=$name -peers=$peer"
+if [[ "$DEBUG" == "true" ]] ; then
+		echo "$name running at UIPort $UIPort and gossipPort $gossipPort"
+fi
+./Peerster -UIPort=$UIPort -gossipAddr=$gossipAddr -name=$name -peers=$peer
