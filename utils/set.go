4a71b65a90c1dcf42425f2a1256f136100c8ceee
Romain Gehrig <romain.gehrig@gmail.com>
2018-10-21 14:51:07 +0200

    Make the string set concurrent safe

--- utils/set.go
+++ utils/set.go
@@ -1,7 +1,12 @@
 package utils
 
+/* Concurrent safe set for Strings */
+
+import "sync"
+
 type StringSet struct {
 	strMap map[string]struct{}
+	lock   *sync.RWMutex
 }
 
 func StringSetInitSingleton(val string) *StringSet {
@@ -9,7 +14,7 @@ func StringSetInitSingleton(val string) *StringSet {
 }
 
 func StringSetInit(vals []string) *StringSet {
-	s := StringSet{make(map[string]struct{})}
+	s := StringSet{strMap: make(map[string]struct{}), lock: &sync.RWMutex{}}
 	for _, val := range vals {
 		s.Add(val)
 	}
@@ -17,16 +22,22 @@ func StringSetInit(vals []string) *StringSet {
 }
 
 func (s *StringSet) Add(val string) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
 	s.strMap[val] = struct{}{}
 }
 
 func (s *StringSet) Has(val string) bool {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
 	_, present := s.strMap[val]
 	return present
 }
 
 // Adapted from https://stackoverflow.com/a/35810932
 func (s *StringSet) Iterate() <-chan string {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
 	c := make(chan string)
 	go func() {
 		for k, _ := range s.strMap {
