56fb27e55926686830d27907444a94f254b2a353
Romain Gehrig <romain.gehrig@gmail.com>
2018-11-08 00:26:48 +0100

    Create the _SharedDir if it does not exist

--- files/files.go
+++ files/files.go
@@ -4,7 +4,11 @@ import (
 	"crypto/sha256"
 	"fmt"
 	. "github.com/RomainGehrig/Peerster/routing"
+	"os"
+	"path/filepath"
 )
+
+const SHARED_DIR_NAME = "_SharedDir"
 
 type SHA256_HASH [sha256.Size]byte
 
@@ -16,13 +20,36 @@ type File struct {
 }
 
 type FileHandler struct {
-	files   map[SHA256_HASH]File
-	routing *RoutingHandler
+	files     map[SHA256_HASH]*File // Mapping from hashes to their corresponding file
+	sharedDir string
+	routing   *RoutingHandler
 }
 
 func NewFileHandler() *FileHandler {
+	exe, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+
+	sharedDir := filepath.Join(filepath.Dir(exe), SHARED_DIR_NAME)
+	if fileInfo, err := os.Stat(sharedDir); os.IsNotExist(err) {
+		// Dir does not exist: we create it
+		fmt.Println("Creating the", SHARED_DIR_NAME, "directory here:", sharedDir)
+		err = os.Mkdir(sharedDir, 0775)
+		if err != nil {
+			panic(err)
+		}
+	} else if err != nil {
+		// Other error => won't recover
+		panic(err)
+	} else if !fileInfo.IsDir() {
+		// Path points to a file instead of a directory, abort with message
+		panic(fmt.Sprintf("Error: path", sharedDir, "points to a file when it should be a directory! Aborting"))
+	}
+
 	return &FileHandler{
-		files: make(map[SHA256_HASH]File),
+		files:     make(map[SHA256_HASH]*File),
+		sharedDir: sharedDir,
 	}
 }
 
