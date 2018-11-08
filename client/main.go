5ab489bdfdcbf46b1026538b055b68b1a96cd33e
Romain Gehrig <romain.gehrig@gmail.com>
2018-11-08 09:44:30 +0100

    Update CLI to enable file indexing request

--- client/main.go
+++ client/main.go
@@ -16,14 +16,31 @@ func main() {
 	}
 
 	uiPort := flag.String("UIPort", "8080", "port for the UI client")
+	dest := flag.String("Dest", "", "destination for the private message")
+	file := flag.String("file", "", "file to be indexed by the gossiper")
 	msg := flag.String("msg", "", "message to be sent")
-	dest := flag.String("dest", "", "destination for the private message")
 
 	flag.Parse()
 
 	address := fmt.Sprintf(":%s", *uiPort)
 
-	toSend := &Request{Post: &PostRequest{Message: &Message{Text: *msg, Dest: *dest}}}
+	postReq := &PostRequest{}
+	toSend := &Request{Post: postReq}
+	// Be careful with the order of the conditions: most restrictive first, then more general
+	switch {
+	case *msg != "" && *dest != "":
+		postReq.Message = &Message{Text: *msg, Dest: *dest}
+	case *msg != "":
+		postReq.Message = &Message{Text: *msg}
+	// TODO FileRequest
+	case *file != "":
+		postReq.FileIndex = &FileIndex{Filename: *file}
+	default:
+		fmt.Println("Arguments don't make sense. :(")
+		flag.Usage()
+		os.Exit(1)
+	}
+
 	packetBytes, err := protobuf.Encode(toSend)
 	if err != nil {
 		fmt.Println(err)
