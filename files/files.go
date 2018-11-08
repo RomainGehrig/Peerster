94c6c5286d603dacf8dcc091edb8ff6fd2471e0b
Romain Gehrig <romain.gehrig@gmail.com>
2018-11-08 01:17:24 +0100

    Implement file indexing

--- files/files.go
+++ files/files.go
@@ -4,17 +4,19 @@ import (
 	"crypto/sha256"
 	"fmt"
 	. "github.com/RomainGehrig/Peerster/routing"
+	"io"
 	"os"
 	"path/filepath"
 )
 
 const SHARED_DIR_NAME = "_SharedDir"
+const BYTES_IN_8KB = 8 * 1024 // == 8192 Bytes == 65'536 bits
 
 type SHA256_HASH [sha256.Size]byte
 
 type File struct {
 	Name         string
-	Size         uint32      // May need to upgrade to uint64 if files > 4GiB
+	Size         int64       // Type given by the Stat().Size()
 	Metafile     []byte      // TODO
 	MetafileHash SHA256_HASH // TODO
 }
@@ -44,7 +46,7 @@ func NewFileHandler() *FileHandler {
 		panic(err)
 	} else if !fileInfo.IsDir() {
 		// Path points to a file instead of a directory, abort with message
-		panic(fmt.Sprintf("Error: path", sharedDir, "points to a file when it should be a directory! Aborting"))
+		panic(fmt.Sprint("Error: path", sharedDir, "points to a file when it should be a directory! Aborting"))
 	}
 
 	return &FileHandler{
@@ -58,12 +60,51 @@ func (f *FileHandler) RunFileHandler(routing *RoutingHandler) {
 }
 
 func (f *FileHandler) RequestFileIndexing(filename string) {
-	indexed, err := f.indexFile(filename)
-	if err {
-		fmt.Println(indexed)
+	abspath := filepath.Join(f.sharedDir, filename)
+	indexed, err := f.toIndexedFile(abspath)
+	if err != nil {
+		fmt.Println("File", abspath, "does not exist. Won't index it.")
+		return
 	}
+	// Put back the filename as Name instead of the abspath
+	indexed.Name = filename
+
+	f.files[indexed.MetafileHash] = indexed
 }
 
-func (f *FileHandler) indexFile(filename string) (*File, bool) {
-	return nil, false
+/* Resulting *File has its absolute path as Name. */
+func (f *FileHandler) toIndexedFile(abspath string) (*File, error) {
+	// Open file
+	file, err := os.Open(abspath)
+	if err != nil {
+		return nil, err
+	}
+	fileStats, _ := file.Stat()
+
+	// Read in chunk of 8KB
+	metafile := make([]byte, 0)
+	chunk := make([]byte, BYTES_IN_8KB)
+	for {
+		read, err := file.Read(chunk)
+		if err != nil {
+			if err != io.EOF {
+				return nil, err
+			}
+			break
+		}
+		// Hash chunk and append it to metafile
+		hash := sha256.Sum256(chunk[:read])
+		metafile = append(metafile, hash[:]...)
+	}
+
+	metafileHash := sha256.Sum256(metafile)
+
+	indexedFile := &File{
+		Name:         abspath, // Change if you want
+		Size:         fileStats.Size(),
+		Metafile:     metafile,
+		MetafileHash: metafileHash,
+	}
+
+	return indexedFile, nil
 }
