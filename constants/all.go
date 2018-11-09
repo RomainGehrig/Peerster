caee6bcd606ef82be1a7229d9810f7cb93198737
Romain Gehrig <romain.gehrig@gmail.com>
2018-11-09 23:39:19 +0100

    Move the ToHash function to utils

--- constants/all.go
+++ constants/all.go
@@ -1,9 +1,15 @@
 package constants
 
+import "crypto/sha256"
+
 const UDP_DATAGRAM_MAX_SIZE int = 65507 // Maximum payload for UDP
 
 // Should be at least 2 for the gossiper to work
 const CHANNEL_BUFFERSIZE int = 16
+
+const DEFAULT_HOP_LIMIT = 10
+
+type SHA256_HASH [sha256.Size]byte
 
 // TODO Put somewhere else ?
 type RegistrationMessageType int
--- utils/hash.go
+++ utils/hash.go
@@ -1,12 +1,13 @@
-package files
+package utils
 
 import (
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	. "github.com/RomainGehrig/Peerster/constants"
 )
 
-func toHash(hashValue []byte) (ret SHA256_HASH, err error) {
+func ToHash(hashValue []byte) (ret SHA256_HASH, err error) {
 	if len(hashValue) != sha256.Size {
 		return ret, errors.New(fmt.Sprint("Cannot convert byte array of length ", len(hashValue), " to a hash of length ", sha256.Size))
 	}
