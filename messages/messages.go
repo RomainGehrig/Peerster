050d793224fb9a14320facd6d353f0b8c1387916
Romain Gehrig <romain.gehrig@gmail.com>
2018-12-01 10:44:13 +0100

    Add data structures for file search

--- messages/messages.go
+++ messages/messages.go
@@ -44,6 +44,27 @@ type DataReply struct {
 	Data        []byte
 }
 
+/// File search
+type SearchRequest struct {
+	Origin   string
+	Budget   uint64
+	Keywords []string
+}
+
+type SearchReply struct {
+	Origin      string
+	Destination string
+	HopLimit    uint32
+	Results     []*SearchResult
+}
+
+type SearchResult struct {
+	FileName     string
+	MetafileHash []byte
+	ChunkMap     []uint64
+	ChunkCount   uint64
+}
+
 /// Private messages
 type PrivateMessage struct {
 	Origin      string `json:"origin"`
@@ -55,12 +76,14 @@ type PrivateMessage struct {
 
 //// Actual packet sent
 type GossipPacket struct {
-	Simple      *SimpleMessage
-	Rumor       *RumorMessage
-	Status      *StatusPacket
-	Private     *PrivateMessage
-	DataRequest *DataRequest
-	DataReply   *DataReply
+	Simple        *SimpleMessage
+	Rumor         *RumorMessage
+	Status        *StatusPacket
+	Private       *PrivateMessage
+	DataRequest   *DataRequest
+	DataReply     *DataReply
+	SearchRequest *SearchRequest
+	SearchReply   *SearchReply
 }
 
 /// Print functions
